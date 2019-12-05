e4cdcfd2d5b124a89c6d56fc9ac76e46e6368e74
albfernandes <deh.oo@hotmail.com>
2019-12-04 22:19:13 -0300

    added TODO: refactor addRule function

--- src/controllers/crudController.js
+++ src/controllers/crudController.js
@@ -3,6 +3,7 @@ const crudController = module.exports = {};
 const dailyPattern = /diariamente/i;
 const datePattern = /\//;
 
+//TODO: refactor this function. To many loops cause a high complexity
 crudController.addRule = (req, res) => {
     try {
         const id = (new Date()).getTime().toString(36) + Math.random().toString(36).slice(2);
